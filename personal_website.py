9efaf2509d297f3bc551ccd934acbb22fae7aa65
drpykachu <156392352+drpykachu@users.noreply.github.com>
2024-01-16 16:57:34 -0800

    d

---
+++ personal_website.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/publications/')
+    return render_template('publications_tab.html')
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0',port=80)
