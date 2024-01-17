1ae14f8122a9d31545a5d5c2700756e88fb7a04d
drpykachu <156392352+drpykachu@users.noreply.github.com>
2024-01-17 09:58:24 -0800

    sd

--- personal_website.py
+++ personal_website.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from flask_talisman import Talisman
 
 app = Flask(__name__)
+talisman = Talisman(app)
 
 @app.route('/')
 def index():
@@ -11,4 +13,5 @@ def publications():
      return render_template('publications_tab.html')
 
 if __name__ == '__main__':
-    app.run(debug=False,host='0.0.0.0',port=80)
+    app.run(debug=False,host='0.0.0.0',port=8000)
+# i think this is good
