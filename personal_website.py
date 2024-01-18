87febb0b6dce7133906ce7fe0f0cd81f62057a4c
drpykachu <156392352+drpykachu@users.noreply.github.com>
2024-01-17 18:30:08 -0800

    w

--- personal_website.py
+++ personal_website.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_talisman import Talisman
 
 app = Flask(__name__)
-talisman = Talisman(app)
+talisman = Talisman(app, content_security_policy=None)
 
 @app.route('/')
 def index():
@@ -13,5 +13,5 @@ def publications():
      return render_template('publications_tab.html')
 
 if __name__ == '__main__':
-    app.run(ssl_context='adhoc', debug=True,port = 8000)
+    app.run(ssl_context=('/etc/letsencrypt/live/anthonyrpyka.servebeer.com.pem', '/etc/letsencrypt/live/anthonyrpyka.servebeer.com/privkey.pem'), debug=True,port = 8000)
 # i think this is good 2
