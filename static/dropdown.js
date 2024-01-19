9b8f32a94543c85c3ef45e23d8b9e7058ecb8643
drpykachu <156392352+drpykachu@users.noreply.github.com>
2024-01-18 18:52:38 -0800

    sd

--- static/dropdown.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const bannerContainer = document.querySelector('.banner-container');
-  const toggleButton = document.getElementById('toggleButton');
-
-  toggleButton.addEventListener('click', function () {
-    bannerContainer.classList.toggle('active');
-  });
-});
